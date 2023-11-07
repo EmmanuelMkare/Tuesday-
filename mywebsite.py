7cbca8ac9669774a851aace151409a0d5aca9474
Emmanuel Mkare <mccareyemmanuel@gmail.com>
2023-11-07 17:25:33 +0300

    added 3 files

---
+++ mywebsite.py
@@ -0,0 +1,48 @@
+<form action="filename">
+    <input type="button" onclick="alert('Welcome to Emmanuel Mkare Website!')"value="Click me!">
+    
+    <lable>Search:</lable>
+    <input type="search" name="mysearch">
+
+    <label>First Name</label><br>
+    <input type="text" name="firstname"><br>
+    <label>Last Name</label><br>
+    <input type="text" name="Lastname">
+    
+    <label>Enter Your Mobile Number:</label>
+    <input type="tel" name="mobilenumber" placeholder="0123-456-789">
+
+    <label>Your password</label><br>
+    <input type="password" name="password">
+ 
+    <label>Select a Date:</label>
+    <input type="date" name="date">
+
+     <label>Select a Time:</label>
+    <input type="time" name="mytime">
+    
+    <lable>Enter a Number (Between 1 to 10):</label>
+    <input type="number"name="mynumber" min="1" max="10">
+
+    <label>Enter Your Website URL:</lable>
+    <input type="url" name="myurl">
+
+    <label>Select a file:</lable>
+    <input type="file" name="myfile">
+
+    <label>Select a Range:</label>
+    <input type="range" name="mynum" min"1" max="20">
+    
+    <a href="https://www.linkedin.com/in/Emmanuel-mccarey-52aaaa291" tabindex="1">My Linkedln Account</a>
+    <a href="https://github.com/EmmanuelMkare/EmmanuelMkare.git" tabindex="1">My Github Account</a>
+    <a href="https://tutoriadeep.com/" tabindex="1">Tutorialdeep</a>
+    <a href="https://tutoriadeep.com/html_tutorial/html-tutorial/" tabindex="1">HTML Tutorials</a>
+    <a href="https://github.com/settings/connections/applications/7d304be23fde467bd231" tabindex="1">My Github setting</a>
+    <a href="https://www.selfgaming.com/" tabindex="1">Selfgaming site</a>
+    <a href="https://powerlearnproject.org/" tabindex="1">Power learn Project</a>
+    <a href="https://plpacademy.powerlearnproject.org/" tabindex="1">PLP Academy</a>
+
+    <input type="submit" value="submit">
+    <input type="image" src="button-tick.png" alt="submit button image" width="50" height="50">
+    <input type="reset">
+</form>
